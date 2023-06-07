24d093799cfc6d3b0e731fd6567ec39d9baf6c7c
hayatemraan <emraanhasanhayat@gmail.com>
2023-06-07 12:12:29 +0600

    helmet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Header from "./shared/Header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "./shared/Footer/Footer";
 
 const App = () => {
   return (
     <div>
-      
+      <Helmet>
+        <title>React App</title>
+      </Helmet>
+      <Header />
+      <div className="min-h-[calc(100vh-120px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Routes } from "./Routes/Routes.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={Routes}></RouterProvider>
+  <AuthProvider>
+    <HelmetProvider>
+      <RouterProvider router={Routes}></RouterProvider>
+    </HelmetProvider>
+  </AuthProvider>
 );
